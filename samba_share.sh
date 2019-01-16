13a8462f9c9eb77b592107902a8ac29d476aefb8
fcocc77 <40272695+fcocc77@users.noreply.github.com>
2019-01-16 16:01:22 -0300

    Modificaciones

--- samba_share.sh
+++ samba_share.sh
@@ -1,75 +1,33 @@
-#============================ instalar sasmba ==============================
+#!/bin/sh
+#----------------------
+user=server5
+shared="/media/server_01"
+#----------------------
 
-yum install samba samba-client samba-common
+name=$(basename $shared)
 
+yum -y install samba samba-client samba-common
+smbpasswd -a $user
 
-
-
-#============================ habilitar samba ==============================
-
--------centos_7---------
-systemctl enable smb
-systemctl enable nmb
-
-
--------centos_6---------
-chkconfig smb on
-chkconfig nmb on
-
-firewall-cmd --permanent --zone=public --add-service=samba
-firewall-cmd --reload
-
-
-#============================ crear clave samba ==============================
-
-useradd "user"
-smbpasswd -a "user"
-
-userdel -r "user"
-
-#============================ editar samba conf ==============================
-
-nano /etc/samba/smb.conf
- 
-
-[global]
+echo "[global]
 workgroup = WORKGROUP
 server string = Samba Server %v
-netbios name = "servername"
 security = user
 map to guest = bad user
 dns proxy = no
 
-#link
-follow symlinks = yes
-wide links = yes
-unix extensions = no
-#-----------------
-
-#multichannel
-server multi channel support = yes
-aio read size = 1
-aio write size = 1
-#-------------------------------------
-
-
-["name disk"]
-path = "/path"
+[$name]
+path = $shared
 browsable =yes
 writable = yes
 guest ok = no
 read only = no
 create mask = 0777
-directory mask = 0777
+directory mask = 0777" > /etc/samba/smb.conf
 
+chcon -Rt samba_share_t $shared
 
-     
-#============================ habilitar ruta ==============================
-
-chcon -Rt samba_share_t "path"
-
-
-#============================ reiniciar servicios ==============================
-
-service smb restart
-service nmb restart
+systemctl start smb
+systemctl start nmb
+systemctl enable smb
+systemctl enable nmb
--- vncserver.sh
+++ vncserver.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 yum -y install tigervnc-server
 
 pkill Xvnc
