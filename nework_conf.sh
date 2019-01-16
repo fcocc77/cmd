e40995d90c877694d9fc711e2a738b5c37986972
Server-05 <fcocc77@gmail.com>
2019-01-16 09:46:36 -0500

    Modificacion network_conf

--- nework_conf.sh
+++ nework_conf.sh
@@ -1,30 +1,19 @@
-/etc/sysconfig/network-scripts/ifcfg-eth0
+NAME=RedLocal
+DEVICE=enp2s0
+TYPE=Ethernet
+IPADDR=192.168.10.49
+NETMASK=255.255.255.0
+GATEWAY=192.168.10.1
+DNS1=8.8.8.8
+ONBOOT=yes
 
-	NAME="RedLocal"
-	DEVICE="eth0"
-	TYPE=Ethernet
+NAME=RedPublica
+DEVICE=enp7s1
+TYPE=Ethernet
+IPADDR=186.10.7.180 
+NETMASK=255.255.255.248
+GATEWAY=186.10.7.177
+DNS1=8.8.8.8
+ONBOOT=yes
 
-	IPADDR=192.168.10.49
-	NETMASK=255.255.255.0
-	GATEWAY=192.168.10.1
-
-	DNS1=8.8.8.8
-
-
-/etc/sysconfig/network-scripts/ifcfg-eth1
-
-	NAME="RedPublica"
-	DEVICE="eth1"
-	TYPE=Ethernet
-
-	IPADDR=186.10.7.178 
-	NETMASK=255.255.255.248
-	GATEWAY=186.10.7.177
-
-	DNS1=8.8.8.8
-
-
-
-
-
-service network restart
+systemctl restart network
