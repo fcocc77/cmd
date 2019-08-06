c64429173a7234fcd8d984f7b18f1fbd54358c3c
pancho <pancho@localhost.localdomain>
2019-08-06 16:23:21 -0400

    ...

--- centos_7_environment/finished.sh
+++ centos_7_environment/finished.sh
@@ -1,6 +1,9 @@
+user=pancho
+
+
 # Virtual Box
 /sbin/vboxconfig
 # -------------------------
 
 # Para guardar los valores "dconf dump /org/mate/ > mate_desktop.ini"
-dconf load /org/mate/ < mate_desktop.ini
+sudo -u $user dconf load /org/mate/ < mate_desktop.ini
