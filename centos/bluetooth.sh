4a8adb07c310f9d343e2e57897631c7a423b53d2
Francisco Notebook <fcocc77@gmail.com>
2019-08-09 13:48:30 -0400

    ...

--- centos/bluetooth.sh
+++ centos/bluetooth.sh
@@ -1,20 +1,30 @@
+#!/usr/bin/env sh
+# Puias repositorio: https://gist.github.com/yuikns/704cc2d672624e5c56d7
+cat << EOF >> /etc/yum.repos.d/puias.repo
+[puias_unsupported]
+name=PUIAS unsupported Base $releasever - $basearch
+mirrorlist=http://puias.math.ias.edu/data/puias/unsupported/$releasever/$basearch/mirrorlist
+enabled=1
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puias
+EOF
+
+rpm --import http://springdale.math.ias.edu/data/puias/7/x86_64/os/RPM-GPG-KEY-puias
+# --------------------------------------------------
+
+# blueman repositorio
 cd '/etc/yum.repos.d/'
 curl 'https://copr.fedorainfracloud.org/coprs/scx/blueman/repo/epel-7/scx-blueman-epel-7.repo' > 'scx-blueman-epel-7.repo'
+# ------------------------------
 
 # Dependencias de blueman
-yum -y install \
-    python36 \
-    python36-gobject \
-    python36-dbus \
+yum -y install python34-gobject blueman
 # ----------------------------
 
-yumGet=$(yumdownloader --urls blueman) # Obtiene la url del paquete
-url=$(echo $yumGet | sed 's/.* //') # Split de espacio y obtiene el ultimo item
-
-# Instala blueman sin la dependencia de python3.4
-rpm -Uvh $url --nodeps
-# -------------------------------
-
-# El rpm de blueman instala la libreria de blueman en python 3.4 asi que ahora se mueve hacia la python 3.6
-mv /usr/lib/python3.4/site-packages/blueman /usr/lib/python3.6/site-packages/
-# -----------------------------------------
+# Cambia el env de python3 a python3.4 para que no de conflicto con el python3.6
+cd /usr/bin/
+bluemans="blueman-adapters blueman-applet blueman-assistant blueman-report blueman-sendto blueman-services"
+for i in $bluemans; do
+    sed -i 's/python3/python3.4/g' $i
+done
+# ---------------------------------
