0ebd401ccbd08457f3deeaaeb7dd758d3dac76a4
Francisco Notebook <fcocc77@gmail.com>
2019-08-09 12:33:04 -0400

    script para instalar blueman (bluetooth)

--- centos/bluetooth.sh
+++ centos/bluetooth.sh
@@ -1,9 +1,20 @@
-# ref : http://springdale.math.ias.edu/wiki/YumRepositories6
-# to impport gpg-key , you should :
-# curl -o /etc/pki/rpm-gpg/RPM-GPG-KEY-puias http://springdale.math.ias.edu/data/puias/7/x86_64/os/RPM-GPG-KEY-puias
-# or :
-# rpm --import http://springdale.math.ias.edu/data/puias/7/x86_64/os/RPM-GPG-KEY-puias
+cd '/etc/yum.repos.d/'
+curl 'https://copr.fedorainfracloud.org/coprs/scx/blueman/repo/epel-7/scx-blueman-epel-7.repo' > 'scx-blueman-epel-7.repo'
 
-# additional , you may also use :
-# curl -o /etc/pki/rpm-gpg/RPM-GPG-KEY-springdale http://springdale.math.ias.edu/data/puias/7/x86_64/os/RPM-GPG-KEY-springdale
+# Dependencias de blueman
+yum -y install \
+    python36 \
+    python36-gobject \
+    python36-dbus \
+# ----------------------------
 
+yumGet=$(yumdownloader --urls blueman) # Obtiene la url del paquete
+url=$(echo $yumGet | sed 's/.* //') # Split de espacio y obtiene el ultimo item
+
+# Instala blueman sin la dependencia de python3.4
+rpm -Uvh $url --nodeps
+# -------------------------------
+
+# El rpm de blueman instala la libreria de blueman en python 3.4 asi que ahora se mueve hacia la python 3.6
+mv /usr/lib/python3.4/site-packages/blueman /usr/lib/python3.6/site-packages/
+# -----------------------------------------
