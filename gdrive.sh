11e95554d1c4275425c535720a1ef7e0f6247bed
Francisco <fcocc77@gmail.com>
2019-08-13 18:54:10 -0400

    gdrive script correccion.

--- gdrive.sh
+++ gdrive.sh
@@ -1,10 +1,21 @@
+#!/usr/bin/env sh
 user=pancho
-
 
 # -------------------
 drive="/home/$user/Google Drive"
 # -------------------
-sudo -u $user mkdir "$drive"
+mkdir "$drive" &> /dev/null
 # -------------------
 
-sudo -u $user rclone sync -P "Google Drive:" "$drive"
+# Su hacemos push y borramos archivos equivocados quedan en la papelera de google drive
+# si queremos recuperarlos
+
+if [ $1 == "pull" ]; then
+    rclone sync -P "Google Drive:" "$drive"
+elif [ $1 == "push" ]; then
+    rclone sync -P "$drive" "Google Drive:"
+else
+    echo ------------------------------------------------
+    echo Argumento incorrecto, intente con pull o push.
+    echo ------------------------------------------------
+fi
