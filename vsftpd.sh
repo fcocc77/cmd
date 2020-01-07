b298196f0a4140707066dc7205ef9805fd3957b2
Francisco <fcocc77@gmail.com>
2020-01-07 15:46:17 -0300

    ftp: para acceso global wan

--- vsftpd.sh
+++ vsftpd.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 #------------------------------
 # importante:
-# El servidor donde esta el ftp tiene que tener puesta 
+# El servidor donde esta el ftp tiene que tener puesta
 # la ip del gateway para que se pueda hacer nat desde el router
 #------------------------------
 yum -y install vsftpd
 
-# Esto no funciona en discos montados director la ruta local_root 
+# Esto no funciona en discos montados director la ruta local_root
 # tiene que ser la carpeta padre del disco montado
-user=pdf
-group=server2
+user=server1
+group=server1
 ftpdir=/home/ftp
 #------------------------------
+
+# dominio global wan
+domain="santiago-zone.ddns.net"
+# -----------------------
 
 userdir=$ftpdir/$user
 
@@ -30,9 +34,18 @@ write_enable=YES
 pam_service_name=vsftpd
 listen=YES
 chroot_local_user=YES
-local_root=$ftpdir" > $conf
+local_root=$ftpdir" >$conf
 
-echo SELINUX=disabled > /etc/selinux/config
+# pasv es para que funcione en una ip global "wan", en el nat del firewall
+# abrir puertos 21 y un rango de 10090-10100
+echo "pasv_enable=YES
+pasv_min_port=10090
+pasv_max_port=10100
+pasv_addr_resolve=YES
+pasv_address=$domain" >>$conf
+# ---------------------------------
+
+echo SELINUX=disabled >/etc/selinux/config
 setenforce 0
 
 systemctl restart vsftpd
