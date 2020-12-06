73b86cc892ed06046f146e5dc90ebf621b3928d2
Francisco Contreras <fcocc77@gmail.com>
2020-12-06 19:17:49 -0300

    ...

--- installer/tmux2.sh
+++ installer/tmux2.sh
@@ -1,2 +1,5 @@
 yum -y https://packages.endpoint.com/rhel/7/os/x86_64/endpoint-repo-1.7-1.x86_64.rpm
 yum -y install tmux
+
+# Administrador de Plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
