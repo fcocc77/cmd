f8b23c240ee2c8dd90f1dc37edf680b4d6efeac5
pancho <pancho@localhost.localdomain>
2020-12-17 14:14:03 -0500

    todo tasks: init

--- installer/djv.sh
+++ installer/djv.sh
@@ -1,2 +1,2 @@
 # Instalacion de djv viewer
-yum -y install https://ufpr.dl.sourceforge.net/project/djv/djv-stable/1.3.0/DJV-1.3.0-1.x86_64.rpm
+sudo yum -y install https://ufpr.dl.sourceforge.net/project/djv/djv-stable/1.3.0/DJV-1.3.0-1.x86_64.rpm
