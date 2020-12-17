390bc1f6d41daf293b3ae5da7a63c03e7aa53498
pancho <pancho@localhost.localdomain>
2020-12-17 13:44:00 -0500

    ...

--- installer/develop.sh
+++ installer/develop.sh
@@ -3,24 +3,21 @@ curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
 
 # Desarrollo
 packages=(
-    # Complilacion
-    make
-    patch
-    gcc
-    perl
-    xorg-x11-server-devel
-    mtdev-devel
-    dkms
+	make
+	patch
+	gcc
+	perl
+	xorg-x11-server-devel
+	mtdev-devel
+	dkms
+	nodejs
 
-    nodejs
-
-    # Edicion
-    python36
-    python36-pip
+	python3-devel
+	python36
+	python36-pip
 )
 sudo yum -y groupinstall "Development Tools"
 sudo yum -y install ${packages[@]}
-# -------------------
 
 # con esto funciona el qDebug para QT5
 sudo mkdir /etc/xdg/QtProject
