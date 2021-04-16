5ba8676d9a0c6aa877c7a87a08f4bf8f17650194
pancho <pancho@localhost.localdomain>
2021-04-16 17:10:46 -0400

    display_switch: cambios ...

--- scripts/display_switch.sh
+++ scripts/display_switch.sh
@@ -1,24 +1,24 @@
 # Este script permuta entre la pantalla del notebook y la pantalla 'dell' o
-# inicializa con prioridad la pantalla hdmi en el 'xinitrc'
+# inicializa con prioridad la pantalla external en el 'xinitrc'
 
 toggle_file=/tmp/toggle_display
 
-laptop_screen='LVDS'
-laptop_format='1366x768'
+laptop_screen='eDP1'
+laptop_format='1920x1080'
 
-hdmi_screen='HDMI-0'
-hdmi_format='1920x1080'
+external_screen='DP1'
+external_format='2560x1440'
 
 function init_display()
 {
 	# si el monitor HDMI1 esta conectado, deja ese como principal y apaga el del notebook y viceversa
-	hdmi=$(xrandr | grep $hdmi_screen)
-	if [[ $hdmi =~ connected ]]; then
+	external=$(xrandr | grep $external_screen)
+	if [[ $external =~ connected ]]; then
 		echo 0 > $toggle_file
-		xrandr --output $laptop_screen --off --output $hdmi_screen --mode $hdmi_format --primary
+		xrandr --output $laptop_screen --off --output $external_screen --mode $external_format --primary
 	else
 		echo 1 > $toggle_file
-		xrandr --output $hdmi_screen --off --output $laptop_screen --mode $laptop_format --primary
+		xrandr --output $external_screen --off --output $laptop_screen --mode $laptop_format --primary
 	fi
 }
 
@@ -26,10 +26,10 @@ function toggle_display()
 {
 	if [ $(cat $toggle_file) == "0" ]; then
 		echo 1 > $toggle_file
-		xrandr --output $hdmi_screen --off --output $laptop_screen --mode $laptop_format --primary
+		xrandr --output $external_screen --off --output $laptop_screen --mode $laptop_format --primary
 	else
 		echo 0 > $toggle_file
-		xrandr --output $laptop_screen --off --output $hdmi_screen --mode $hdmi_format --primary
+		xrandr --output $laptop_screen --off --output $external_screen --mode $external_format --primary
 	fi
 }
 
