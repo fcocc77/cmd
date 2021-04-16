68b95a853163b27e74a20f76c60073b8bf8ff434
pancho <pancho@localhost.localdomain>
2021-04-15 23:24:16 -0400

    ....

---
+++ drivers/kernel.sh
@@ -0,0 +1,5 @@
+sudo yum -y install https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm
+
+# kernel-lt es la version estable a largo plazo del kernel
+# verificar que se instale el 5.4.112-1
+sudo yum --enablerepo=elrepo-kernel install kernel-lt
