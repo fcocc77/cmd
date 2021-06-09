d5f9aebb87043dce872859e90be8a4e05a6a9eb6
pancho <pancho@localhost.localdomain>
2021-06-09 01:09:12 -0400

    appfinder: correccion

--- scripts/appfinder.sh
+++ scripts/appfinder.sh
@@ -3,3 +3,9 @@ running=$(wmctrl -l|awk '{$3=""; $2=""; $1="";  print $0}')
 if ! [[ $running =~ "Application Finder" ]]; then
     xfce4-appfinder
 fi
+
+if [ $1 == "mate" ]; then
+    wmctrl -r "Application Finder" -b add,above # Mantiene la applicacion siempre sobre
+    xdotool windowactivate $(xdotool search --onlyvisible --class 'xfce4-appfinder' | tail -n1) # Enfoca la applicacion en primer plano
+fi
+
--- scripts/mate_desktop_config.sh
+++ scripts/mate_desktop_config.sh
@@ -31,6 +31,6 @@ add_custom_key() {
     dconf write /org/mate/desktop/keybindings/custom$1/action \'"$4"\'
 }
 
-add_custom_key 0 'app_finder' '<Mod4>o' "sh $path/appfinder.sh"
+add_custom_key 0 'app_finder' '<Mod4>o' "sh $path/appfinder.sh mate"
 add_custom_key 1 'gnome_terminal' '<Shift><Mod4>Return' 'gnome-terminal'
 add_custom_key 2 'chrome' '<Mod4>g' 'google-chrome-stable'
