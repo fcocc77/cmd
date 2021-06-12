7c733955f07cb820ae70d8679376487f026f3fc4
pancho <pancho@localhost.localdomain>
2021-06-12 15:01:02 -0400

    nvidia prime configuracion ok

--- drivers/nvidia.sh
+++ drivers/nvidia.sh
@@ -1,16 +1,3 @@
-
-# BumbleBee
-# 1 - Hay que cambiar en el /etc/bumblebee/bumblebee.conf "Driver=nvidia"
-# 2 - el Display del bumblebee.conf "VirtualDisplay=:8" no puede tener el mismo numero que la
-# pantalla principal (echo $DISPLAY) si no da un error
-# 3 - añadir el usuario al grup de bumblebee y reiniciar: gpasswd -a user bumblebee
-
-# con esto se puede ver el debug:
-#     "optirun -vv glxgears -info"
-
-# copiar xorg nvidia bumblebee
-sudo cp ../xorg/nvidia_bumblebee.conf /etc/bumblebee/xorg.conf.nvidia
-
 # NVIDIA Driver
 # nvidia driver funciona bien con kernel 5.4lt de elrepo
 # 1 - desabilitar nouveau del kernel: rd.driver.blacklist=nouveau nouveau.modeset=0
@@ -19,14 +6,14 @@ sudo cp ../xorg/nvidia_bumblebee.conf /etc/bumblebee/xorg.conf.nvidia
 
 wget https://es.download.nvidia.com/XFree86/Linux-x86_64/460.73.01/NVIDIA-Linux-x86_64-460.73.01.run -P ~/Downloads
 
-
 # desabilitar selinux; sestatus para ver estado
 sudo sed -i "s|SELINUX=enforcing|SELINUX=disabled|g" /etc/selinux/config
 
-# Acceso directo
-echo "[Desktop Entry]
-Name=Nvidia Settings
-Exec=optirun nvidia-settings -c :8.0
-Icon=nvidia-settings
-Type=Application" | sudo tee "/usr/share/applications/nvidia-settings.desktop"
+
+# Configuracion de nvidia-prime
+echo '#!/bin/bash
+__NV_PRIME_RENDER_OFFLOAD=1 __VK_LAYER_NV_optimus=NVIDIA_only __GLX_VENDOR_LIBRARY_NAME=nvidia "$@"' \
+| sudo tee /usr/bin/prime
+
+sudo chmod 755 /usr/bin/prime
 
