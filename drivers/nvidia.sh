22e3ac7ccfd41139a88a0915279a3d4a4c933a78
pancho <pancho@localhost.localdomain>
2021-06-12 18:24:21 -0400

    nvidia.sh: update ...

--- drivers/nvidia.sh
+++ drivers/nvidia.sh
@@ -2,9 +2,16 @@
 # nvidia driver funciona bien con kernel 5.4lt de elrepo
 # 1 - desabilitar nouveau del kernel: rd.driver.blacklist=nouveau nouveau.modeset=0
 # 2 - instalar kernel headers: sudo yum --enablerepo=elrepo-kernel install kernel-lt-headers
-# 3 - visualizar con nvidia-smi
 
 wget https://es.download.nvidia.com/XFree86/Linux-x86_64/460.73.01/NVIDIA-Linux-x86_64-460.73.01.run -P ~/Downloads
+
+# gpu-usage: nvidia-smi
+# info: nvidia-smi -q
+# rendimiento: nvidia-smi -q -d PERFORMANCE
+# temperatura: nvidia-smi -q -d Temperature
+
+# !temperatura nvidia de thinkpad, tiene un limite de 
+# 57 grados, y luego se estrangula la frecuencia
 
 # desabilitar selinux; sestatus para ver estado
 sudo sed -i "s|SELINUX=enforcing|SELINUX=disabled|g" /etc/selinux/config
