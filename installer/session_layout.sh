949cc137cd7be636810df8f986cde6b3c82c7d80
pancho <pancho@localhost.localdomain>
2021-06-13 20:53:52 -0400

    session_layout: init

---
+++ installer/session_layout.sh
@@ -0,0 +1,14 @@
+# script para guardar las posiciones de las ventana en una session
+sudo dnf -y install wmctrl
+
+cd /tmp
+wget https://github.com/arnon-weinberg/session/raw/master/session
+chmod 755 ./session
+sudo mv session /usr/local/bin
+
+mkdir ~/.config/autostart
+
+echo "[Desktop Entry]
+Name=Session Restore
+Exec=session restore missing
+Type=Application" > ~/.config/autostart/session.desktop
