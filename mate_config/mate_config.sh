7acab4170305c74951c78425fcfda650008a4058
pancho <pancho@localhost.localdomain>
2021-06-15 10:08:58 -0400

    mate_config: update shortcuts

--- mate_config/mate_config.sh
+++ mate_config/mate_config.sh
@@ -28,11 +28,11 @@ gsettings set org.mate.Marco.window-keybindings move-to-workspace-2 '<Shift><Mod
 gsettings set org.mate.Marco.window-keybindings move-to-workspace-3 '<Shift><Mod4>3'
 gsettings set org.mate.Marco.window-keybindings move-to-workspace-4 '<Shift><Mod4>4'
 
-gsettings set org.mate.Marco.window-keybindings move-to-side-e '<Control><Mod4>l' # right
-gsettings set org.mate.Marco.window-keybindings move-to-side-s '<Control><Mod4>j' # down
-gsettings set org.mate.Marco.window-keybindings move-to-side-w '<Control><Mod4>h' # left
-gsettings set org.mate.Marco.window-keybindings move-to-side-n '<Control><Mod4>k' # up
-gsettings set org.mate.Marco.window-keybindings move-to-center '<Control><Mod4>semicolon' #semicolon es ;
+gsettings set org.mate.Marco.window-keybindings move-to-side-e '<Alt><Mod4>l' # right
+gsettings set org.mate.Marco.window-keybindings move-to-side-s '<Alt><Mod4>j' # down
+gsettings set org.mate.Marco.window-keybindings move-to-side-w '<Alt><Mod4>h' # left
+gsettings set org.mate.Marco.window-keybindings move-to-side-n '<Alt><Mod4>k' # up
+gsettings set org.mate.Marco.window-keybindings move-to-center '<Alt><Mod4>semicolon' #semicolon es ;
 
 # gsettings set org.mate.Marco.global-keybindings cycle-windows '<Mod4>j'
 gsettings set org.mate.Marco.window-keybindings toggle-maximized '<Mod4>f'
@@ -63,13 +63,13 @@ add_custom_key 4 'logout' '<Shift><Mod4>q' 'mate-session-save --logout'
 add_custom_key 5 'tiling' '<Mod4>space' "python3 $path/../python_scripts/tiling.py"
 add_custom_key 6 'pluma' '<Mod4>e' 'pluma'
 
-action 7 'move_left' '<Mod4>h' 'workspace_move_left'
-action 8 'move_right' '<Mod4>l' 'workspace_move_right'
+action 7 'move_left' '<Control><Mod4>h' 'workspace_move_left'
+action 8 'move_right' '<Control><Mod4>l' 'workspace_move_right'
 
-action 9 'to_top_right' '<Alt><Mod4>l' 'to_top_right'
-action 10 'to_top_left' '<Alt><Mod4>h' 'to_top_left'
-action 11 'to_bottom_right' '<Alt><Mod4>k' 'to_bottom_right'
-action 12 'to_bottom_left' '<Alt><Mod4>j' 'to_bottom_left'
+action 9 'to_top_right' '<Alt><Mod4>o' 'to_top_right'
+action 10 'to_top_left' '<Alt><Mod4>y' 'to_top_left'
+action 11 'to_bottom_right' '<Alt><Mod4>period' 'to_bottom_right'
+action 12 'to_bottom_left' '<Alt><Mod4>n' 'to_bottom_left'
 
 action 13 'left_focus' '<Mod4>j' 'left_focus'
 action 14 'right_focus' '<Mod4>k' 'right_focus'
