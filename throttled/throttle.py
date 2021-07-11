938dd4cd82fd0ddb159d3c7a339ad10bcee1deca
pancho <pancho@localhost.localdomain>
2021-07-10 20:46:07 -0400

    throttle: listo

--- throttled/throttle.py
+++ throttled/throttle.py
@@ -1,18 +1,25 @@
 from time import sleep
 import psutil
 import os
-from sys import argv
-
+import time
 
 current_freq = 0
 
+last_change = 0
 
-def set_frequency(freq: float):
-    global current_freq
+def set_frequency(freq: float, force = False):
+    global current_freq, last_change
 
-    if freq == current_freq:
-        return
+    if not force:
+        if freq == current_freq:
+            return
 
+        elapsed = time.time() - last_change
+
+        if elapsed < 7:
+            return
+
+    last_change = time.time()
     current_freq = freq
 
     cmd = 'cpupower frequency-set -u ' + str(freq) + 'GHz'
@@ -24,15 +31,15 @@ while(True):
     temp: int = psutil.sensors_temperatures()["coretemp"][0].current
 
     if (temp > 85):
-        set_frequency(2.0)
+        set_frequency(2.0, True)
 
-    if (temp > 80):
+    elif (temp > 80):
         set_frequency(2.5)
-
-    elif (temp > 75):
-        set_frequency(2.9)
 
     elif (temp < 50):
         set_frequency(5.0)
+
+    else:
+        set_frequency(2.9)
 
     sleep(2)
